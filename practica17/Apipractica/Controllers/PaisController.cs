d3eb2c5523a516a7f329eb88098ddb90aa3991f6
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-03-17 17:07:12 -0400

    Creacion de Controller y programa correcto

---
+++ practica17/Apipractica/Controllers/PaisController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Apipractica.Models;
+
+namespace Apipractica.Controllers
+{
+    public class PaisController : ApiController
+    {
+        private DataContext db = new DataContext();
+
+        // GET: api/Pais
+        public IQueryable<Pais> GetPais()
+        {
+            return db.Pais;
+        }
+
+        // GET: api/Pais/5
+        [ResponseType(typeof(Pais))]
+        public IHttpActionResult GetPais(string id)
+        {
+            Pais pais = db.Pais.Find(id);
+            if (pais == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pais);
+        }
+
+        // PUT: api/Pais/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPais(string id, Pais pais)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != pais.Name)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(pais).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaisExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Pais
+        [ResponseType(typeof(Pais))]
+        public IHttpActionResult PostPais(Pais pais)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Pais.Add(pais);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (PaisExists(pais.Name))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = pais.Name }, pais);
+        }
+
+        // DELETE: api/Pais/5
+        [ResponseType(typeof(Pais))]
+        public IHttpActionResult DeletePais(string id)
+        {
+            Pais pais = db.Pais.Find(id);
+            if (pais == null)
+            {
+                return NotFound();
+            }
+
+            db.Pais.Remove(pais);
+            db.SaveChanges();
+
+            return Ok(pais);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PaisExists(string id)
+        {
+            return db.Pais.Count(e => e.Name == id) > 0;
+        }
+    }
+}
